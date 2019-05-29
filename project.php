e1fcb6aecf3bdc02c96e1f84a2a3b9f1e3776f52
leiidyapop <49569411+leiidyapop@users.noreply.github.com>
2019-05-29 15:45:00 -0400

    Create project.php

---
+++ project.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Project extends Model{
+
+	protected $table = 'projects';
+	public function getDurationAsString()
+ {
+	 return'';
+ }
+
+   public function getTimeAsString()
+ {
+	 return'';
+ }
+}
+
+?>
